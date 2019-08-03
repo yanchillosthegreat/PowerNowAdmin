88d47d4d0a75e25a0f7f7b92dbb2fd9f35145654
yanmoroz <yanmoroz@DESKTOP-97MBDCL>
2019-08-03 22:32:59 +0300

    init commit

---
+++ PowerBankAdmin/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PowerBankAdmin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public int Name { get; set; }
+
+        [BindProperty]
+        public List<int> Ints { get; set; }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Name = 3;
+            Ints = new List<int> { 1, 2, 3 };
+            return Page();
+        }
+
+    }
+}
