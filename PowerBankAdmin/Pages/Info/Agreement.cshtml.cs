f8d739978f44f32d5c234cc0d0732fa1e1bffbcb
yanmoroz <yanmoroz@DESKTOP-97MBDCL>
2019-11-27 05:44:12 +0300

    Agreement Page

---
+++ PowerBankAdmin/Pages/Info/Agreement.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PowerBankAdmin.Pages.Info
+{
+    public class AgreementModel : BaseAuthCostumerPage
+    {
+        public void OnGet()
+        {
+            IdentifyCostumer();
+            ViewData["Title"] = "œŒÀ‹«Œ¬¿“≈À‹— Œ≈ —Œ√À¿ÿ≈Õ»≈";
+        }
+    }
+}
